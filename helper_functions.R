98e85b58d5155d7b4234b27edf62bacb53c6fac5
Christine <ctataru5@stanford.edu>
2019-05-03 15:53:31 -0700

    glmnet for regression, and more clear cca and dpcoa

--- helper_functions.R
+++ helper_functions.R
@@ -9,9 +9,9 @@ library(vegan)
 
 
 loadPhyloseq <- function(dir){
-  ps <- readRDS(file.path(dir, "data/ps.RDS"))
-  ps_dds <- readRDS(file.path(dir, "data/ps_dds.RDS"))
-  return(list(ps, ps_dds))
+  ps <- readRDS(file.path(dir, "data/ps_w_tree.RDS"))
+  #ps_dds <- readRDS(file.path(dir, "data/ps_dds.RDS"))
+  return(ps)
 }
 
 cleanSampleData <- function(ps){
@@ -34,7 +34,7 @@ getCommonOtus <- function(ps, numSamples){
  # ps_filt <- prune_samples(colSums(otu_table(ps_filt)) > 0, ps_filt) #Drop samples that are empty
   
   names <- apply(tax_table(ps_filt), 1, function(phy_names){
-    return(paste(tax_table(ps_filt)[1,], collapse = "_"))
+    return(paste(phy_names[[5]], phy_names[[6]], sep = "_"))
   })
   
   taxa_names(ps_filt) <- make.unique(names)
@@ -132,30 +132,34 @@ getFormula <- function(x, y){
 }
 
 
-regression <- function(y, input, method ){
+regression <- function(y, input, method, reg_strength){
   #input will have all input variables and the target
   x <- colnames(input)[colnames(input) != y]
   
   if(method == "cvglmnet"){
     target <- input[,y]
     input <- select(input, -y)
-    fit <- cv.glmnet(as.matrix(input), target,family = "gaussian")
-    train_preds = predict(fit, as.matrix(input), s = fit$lambda[length(fit$lambda)])
+    fit <- cv.glmnet(as.matrix(input), target, family = "gaussian", alpha = 0.95)
+    lambda = fit$lambda[length(fit$lambda)]
+    train_preds = predict(fit, as.matrix(input), s = lambda)
+    print(train_preds)
     fit$mse = sum((train_preds - target)^2)
-    print(fit$mse)
+    
+    #Save lambda of choice in model
+    #Deal with the regularization strength issue
+    if(reg_strength %in% c("lambda.min", "lambda.1se")){
+      fit$lambda_chose <- reg_strength
+    }
+    else{
+      #Full reg strength is 1, no reg strength is 0
+      lambda = fit$lambda[round(length(fit$lambda) * (1-reg_strength))]
+      fit$lambda_chose <- lambda
+    }
   }
   if(method == "basic"){
     form <- getFormula(x, y)
     fit <- lm(form, input)
     fit$rval <- summary(fit)$adj.r.squared
-  }
-  if(method == "glmnet"){
-    input <- input[,x]
-    target <- input[,y]
-    fit <- glmnet(x = as.matrix(input), target, family = "gaussian")
-    num_lamb <- length(fit$lambda)
-    rsquared <- fit$dev.ratio[num_lamb]
-    print(rsquared)
   }
   
   return(fit)
@@ -163,9 +167,10 @@ regression <- function(y, input, method ){
 
 
 
-regression_randomNoise <- function(y, df, otu, method, num_vars, permutations = 10){
-  other <- getOther(y)
-  df_use <- select(df, -c(other))
+regression_randomNoise <- function(y, df, otu, method, num_vars, permutations = 10, reg_strength){
+  #other <- getOther(y)
+  #df_use <- select(df, -c(other))
+  df_use <- df
   
   set.seed(0)
   rsquared_cum <- c()
@@ -183,35 +188,10 @@ regression_randomNoise <- function(y, df, otu, method, num_vars, permutations = 
       numRands = numRands + 1
     }
     
-    x <- colnames(df_use)
-    x <- x[x != y]
-    form <- getFormula(x, y)
-    df_stand <- as.data.frame(apply(df_use, 2, function(var) return( (var - mean(var, na.rm = T)) / sd(var, na.rm = T))))
+    df_stand <- standardize(df_use)
     
-    if(method == "glm"){
-      fit <- glm(form, df_stand, family = "gaussian")
-      print(fit$aic)
-    }
-    if(method == "basic"){
-      fit <- lm(form, df_stand)
-      rsquared <- summary(fit)$adj.r.squared
-    }
-    if(method == "glmnet"){
-      input <- df_stand[,x]
-      target <- df_stand[,y]
-      fit <- glmnet(x = as.matrix(input), target, family = "gaussian")
-      num_lamb <- length(fit$lambda)
-      rsquared <- fit$dev.ratio[num_lamb]
-      print(rsquared)
-    }
-    if(method == "cvglmnet"){
-      input <- df_stand[,x]
-      target <- df_stand[,y]
-      fit <- cv.glmnet(as.matrix(input), target,family = "gaussian")
-      train_preds = predict(fit, as.matrix(input), s = fit$lambda[length(fit$lambda) - 5])
-      fit$mse = sum((train_preds - target)^2)
-      rsquared <- fit$mse
-    }
+    model <- regression(y = y, input = df_stand, method = method, reg_strength)
+    rsquared <- getRvalues(method = method, model = model, reg_strength = reg_strength)
     rsquared_cum <- c(rsquared_cum, rsquared)
   }
   
@@ -229,8 +209,8 @@ plotVariationExplained <- function(df, title = "", ylab = 'var_exp'){
   p <- p + geom_text(aes(label=value), vjust = -0.3, size = 10)
   if(ylab == 'var_exp'){
     p <- p + ylab("Variance explained (R^2)")
-  }else if(ylab == 'mse'){
-    p <- p + ylab("Mean Squared Error")
+  }else if(ylab == 'dev.ratio'){
+    p <- p + ylab("Fraction of null deviance explained")
   }
   
   p <- p + ggtitle(title)
@@ -243,13 +223,14 @@ getSigCoefs <- function(model, method){
     return(coefs[coefs[,4]  < .07, , drop = F])
   }
   if(method == "cvglmnet"){
-    return(coefficients(model))
+    return(coef(model, s = model$lambda_chose)[as.numeric(coef(model, s= model$lambda_chose)) != 0, ])
   }
 
 }
 
 
-getRvalues <- function(method, model, s='lambda.min'){
+getRvalues <- function(method, model, reg_strength){
+  
   if(method=="glmnet"){
     num_lamb <- 30
     return(model$dev.ratio[num_lamb])
@@ -257,15 +238,26 @@ getRvalues <- function(method, model, s='lambda.min'){
   if(method == "basic"){
     return(summary(model)$adj.r.squared)
   }
-  #if(method == "cvglmnet"){
-  #  return(model$mse)
-  #}
+
   if(method == "cvglmnet"){
-    return(model$glmnet.fit$dev.ratio[which(model$lambda == model[[s]])])
+    
+    #Deal with the regularization strength issue
+    if(reg_strength %in% c("lambda.min", "lambda.1se")){
+      return(model$glmnet.fit$dev.ratio[which(model$lambda == model[[reg_strength]])])
+    }
+    #If lambda.min and lambda.1se are not allowing us to fit a model at all, we lower the regularization manually
+    else{
+      #Full reg strength is 1, no reg strength is 0
+      lambda = model$lambda[round(length(model$lambda) * (1-reg_strength))]
+      dev_exp <- model$glmnet.fit$dev.ratio[which(model$lambda == lambda)]
+      print(dev_exp)
+      return(dev_exp) #% deviance explained
+    }
+
   }
 }
 
-runLinearRegression <- function(y, ps, method, permutations, title = ""){
+runLinearRegression <- function(y, ps, method, permutations, reg_strength, title = ""){
   
   #Get data together
   basic_input <- getBasicInput(ps, y = y)
@@ -275,10 +267,10 @@ runLinearRegression <- function(y, ps, method, permutations, title = ""){
   
   #Run regression on each dataset
   set.seed(1)
-  basic <- regression(y, basic_input, method)
-  basic_merc <- regression(y, merc_input, method)
-  basic_otu <- regression(y, otu_input, method)
-  basic_all <- regression(y, all_input, method)
+  basic <- regression(y, basic_input, method, reg_strength)
+  basic_merc <- regression(y, merc_input, method, reg_strength)
+  basic_otu <- regression(y, otu_input, method, reg_strength)
+  basic_all <- regression(y, all_input, method, reg_strength)
   
   #Make predictions using variables of random noise
   df <- sample_data(ps)
@@ -286,43 +278,52 @@ runLinearRegression <- function(y, ps, method, permutations, title = ""){
   if(nrow(otu_mat) != nsamples(ps)){
     otu_mat <- t(otu_mat)
   }
-  random1 = regression_randomNoise(y, df, otu_mat, method, num_vars = 3,  permutations = permutations)
   
-  random2 = regression_randomNoise(y, df, otu_mat, method, num_vars = ntaxa(ps), permutations = permutations)
+  #Deal with regularization parameter
+
+  random1 = regression_randomNoise(y, basic_input, otu_mat, method, num_vars = 3,  permutations = permutations, reg_strength)
   
-  random3 = regression_randomNoise(y, df, otu_mat, method, num_vars = ntaxa(ps) + 3, permutations)
+  random2 = regression_randomNoise(y, basic_input, otu_mat, method, num_vars = ntaxa(ps), permutations = permutations, reg_strength = reg_strength)
+  
+  random3 = regression_randomNoise(y, basic_input, otu_mat, method, num_vars = ntaxa(ps) + 3, permutations, reg_strength = reg_strength)
   
   #Plot results
   ylab = "var_exp"
   if(method == "cvglmnet"){
-    ylab= "mse"
+    ylab= "dev.ratio"
   }
+  
+  
   plotVariationExplained(data.frame(
-    basic = getRvalues(method, basic),
-    basic_merc = getRvalues(method, basic_merc), 
-    basic_micro = getRvalues(method, basic_otu),
-    basic_micro_merc = getRvalues(method, basic_all),
-    rand1 = random1, rand1 = random2, rand3 = random3),
+    basic = getRvalues(method, basic, reg_strength),
+    basic_merc = getRvalues(method, basic_merc, reg_strength), 
+    basic_micro = getRvalues(method, basic_otu, reg_strength),
+    basic_micro_merc = getRvalues(method, basic_all, reg_strength),
+    rand1 = random1, rand2 = random2, rand3 = random3),
     title = title, ylab = ylab)
 
   
   print("Basic Model: ")
-  print(getSigCoefs(basic, method))
+  sig_vars_basic <- getSigCoefs(basic, method)
+  print(sig_vars_basic)
   
   print("Basic + Merc Model: ")
-  print(getSigCoefs(basic_merc, method))
+  sig_vars_merc <- getSigCoefs(basic_merc, method)
+  print(sig_vars_merc)
   
   print("Basic + Micro Model: ")
-  print(getSigCoefs(basic_otu, method))
+  sig_vars_micro <- getSigCoefs(basic_otu, method)
+  print(sig_vars_micro)
   
   print("Basic + Micro + Merc Model: ")
-  print(getSigCoefs(basic_all, method))
+  sig_vars_all <- getSigCoefs(basic_all, method)
+  print(sig_vars_all)
   return(list(basic, basic_merc, basic_otu, basic_all))
 }
 
 runPermanova <- function(ps, method){
   otu <- otu_table(ps)
-  if(nrow(otu) < ncol(otu)){
+  if(taxa_are_rows(ps)){
     otu <- t(otu) #want samples as rows
   }
   dist_bray <- vegdist(otu, method = method)
@@ -331,32 +332,50 @@ runPermanova <- function(ps, method){
   perm = adonis2(form, data = as.data.frame(sample_data(ps)), method = "bray", perm = 9999)
 }
 
-plotCCA <- function(ps, color){
-  vars <- paste(colnames(sample_data(ps)), collapse = "+")
+recordSignificance <- function(ps, model, method, column_name){
+  sig_vars_micro <- getSigCoefs(model, method = method)
+  
+  tmp_taxa <- as.data.frame(tax_table(ps))
+  
+  if(!(column_name %in% colnames(tmp_taxa))){
+    tmp_taxa[ , column_name] <- as.numeric(rep(0, ntaxa(ps)))
+  }
+  sig_taxa <- names(sig_vars_micro)[names(sig_vars_micro) %in% taxa_names(ps)]
+  tmp_taxa[ , column_name] <- as.numeric(as.character(tmp_taxa[ , column_name]))
+  tmp_taxa[sig_taxa, column_name] <- ifelse(sig_vars_micro[sig_taxa] > 0, "pos", "neg")
+  tax_table(ps) <- tax_table(as.matrix(tmp_taxa))
+  return(ps)
+}
+
+
+plotCCA <- function(ps, dist, color, shape, type ){
+  vars <- paste(colnames(sample_data(ps)), collapse = "+") #Coordinate system
   ind <- paste("~")
   form <- formula(paste(ind, vars, sep = ""))
-  cca <- ordinate(physeq = ps, method = "CAP", distance = dist, formula = form)
+  #Place samples based on distances between their microbiomes
+  cca <- ordinate(physeq = ps, method = "CCA", distance = dist, formula = form)
+  cap_plot <- plot_ordination(ps, 
+                              ordination = cca,
+                              type = type, 
+                              color= color, 
+                              shape = shape, 
+                              axes = c(1,2)) + geom_point(size=5)
+ 
   
-  cap_plot <- plot_ordination(physeq = ps, 
-                              ordination = cca, 
-                              color = color, 
-                              axes = c(1,2))
-  
-  cap_plot <- cap_plot + geom_point(size = 5)
   arrowmat <- vegan::scores(cca, display = "bp")
   # Add labels, make a data.frame
   arrowdf <- data.frame(labels = rownames(arrowmat), arrowmat)
   # Define the arrow aesthetic mapping
-  arrow_map <- aes(xend = CAP1 * 3, 
-                   yend = CAP2 * 3, 
+  arrow_map <- aes(xend = CCA1 * 4, 
+                   yend = CCA2 * 4, 
                    x = 0, 
                    y = 0, 
                    shape = NULL, 
                    color = NULL, 
                    label = labels)
   
-  label_map <- aes(x = 3.5 * CAP1, 
-                   y = 3.5 * CAP2, 
+  label_map <- aes(x = 4.5 * CCA1, 
+                   y = 4.5 * CCA2, 
                    shape = NULL, 
                    color = NULL, 
                    label = labels)
@@ -372,7 +391,7 @@ plotCCA <- function(ps, color){
       color = "gray", 
       arrow = arrowhead
     ) + 
-    geom_text(
+    geom_text_repel(
       mapping = label_map, 
       size = 4,  
       data = arrowdf, 
@@ -381,3 +400,4 @@ plotCCA <- function(ps, color){
   return(cap_plot)
 }
 
+library(ggrepel)
